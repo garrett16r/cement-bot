6fbac9d3fd156be547e260df597d4fdb1bbbe752
Garrett <garrett16r@gmail.com>
2025-03-01 21:11:29 -0500

    Setup command handlers and /ping command

---
+++ commands/utility/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with "Pong!" to check connection'),
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    },
+};
