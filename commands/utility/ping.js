32e83cdee08d163387d7e17e74d8b995075cc41b
garrett16r <grose11@students.kennesaw.edu>
2025-03-02 16:35:14 -0500

    Make /ping response ephemeral

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with roundtrip latency for debugging'),
     async execute(interaction) {
-        const sent = await interaction.reply('Pinging...');
+        const sent = await interaction.reply({ content: 'Pinging...', flags: MessageFlags.Ephemeral });
         interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
     },
 };
