0acc11eb0af7420d7a851f1a1c259ab2ce966949
Garrett <garrett16r@gmail.com>
2025-03-04 10:59:41 -0500

    Complete /cve command

--- commands/helpful/cve.js
+++ commands/helpful/cve.js
@@ -11,15 +11,36 @@ module.exports = {
         ),
     async execute(interaction) {
         const cveRegex = '^CVE-\\d{4}-\\d{4,}$';
-        const cve = interaction.options.getString('cve_id');
+        let cve = interaction.options.getString('cve_id');
+        let name = '';
+        cve = cve.toUpperCase();
 
         if (!cve.match(cveRegex)) {
             await interaction.reply({ content: `CVE "${cve}" is invalid!\n\n*Example cve_id: CVE-2025-24200*`, flags: MessageFlags.Ephemeral });
             return;
         }
 
-        // Only returns a link to the CVE info currently.
-        // Later, find an API to call for detailed info that can go into an embed
-        await interaction.reply(`https://nvd.nist.gov/vuln/detail/${cve}`);
+        try {
+            const response = await fetch(`https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=${cve}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            // console.log(data.vulnerabilities[0]);
+
+            name = data.vulnerabilities[0].cve.cisaVulnerabilityName ?? 'No name provided';
+            published = data.vulnerabilities[0].cve.published.split('T')[0];
+            vulnStatus = data.vulnerabilities[0].cve.vulnStatus;
+            description = data.vulnerabilities[0].cve.descriptions[0]?.value.trim();
+            baseScore = data.vulnerabilities[0].cve.metrics.cvssMetricV31[0]?.cvssData.baseScore;
+            requiredAction = data.vulnerabilities[0].cve?.cisaRequiredAction ?? 'None';
+        } catch (error) {
+            console.log(error);
+        }
+
+        await interaction.reply(`
+            **${cve}: ${name}**\nPublished: ${published}\nStatus: ${vulnStatus}\n\n**CVSSv3.1**: ${baseScore}\n\n**Description:**\n${description}\n\n**CISA Required Action:**\n${requiredAction}\n\n[Full Info](https://nvd.nist.gov/vuln/detail/${cve})`,
+        );
     },
 };
