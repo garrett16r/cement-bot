d70a23d16aa996312aeb9a26b6f2ed1e81458cf7
Garrett <garrett16r@gmail.com>
2025-03-06 00:02:23 -0500

    Fix /ping description

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with roundtrip latency for debugging'),
+        .setDescription('Test if the bot is alive or died from cement overdose'),
     async execute(interaction) {
         await interaction.reply({ content: 'Pong!', flags: MessageFlags.Ephemeral });
         // interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
